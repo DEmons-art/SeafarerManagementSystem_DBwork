61485afcc079cff74b67e97da7986ad7c2f89847
A <232316671+DEmons-art@users.noreply.github.com>
2026-05-11 19:16:07 +0800

    feat:做了一下交互框架和数据列表展示

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.post('/api/login', (req, res) => {
             console.error('🚨 SQL报错详情:', err); // 👉 加了这一行，把真凶打印到黑窗口里
             return res.status(500).json({ success: false, message: '服务器错误' });
         }
-        
+
         if (err) return res.status(500).json({ success: false, message: '服务器错误' });
         
         if (results.length > 0) {
@@ -49,6 +49,46 @@ app.post('/api/login', (req, res) => {
             res.status(401).json({ success: false, message: '账号或密码错误' });
         }
     });
+
+    app.get('/api/crews', (req, res) => {
+        
+
+    // 写 SQL 语句：去 crew_info 表里查数据，出于安全考虑，一般不把 password 查出来发给前端
+    const sql = 'SELECT id, username, name, gender, id_card, phone, is_at_sea, role FROM crew_info';
+    
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error('查询船员失败:', err);
+            return res.status(500).json({ success: false, message: '获取数据失败' });
+        }
+        // 把查到的结果打包发给前端
+        res.json({ success: true, data: results });
+    });
+});
+
+    // 获取所有出海航次记录 (GET 请求)
+    app.get('/api/voyages', (req, res) => {
+    const sql = `
+        SELECT 
+            v.record_id, 
+            c.name AS crew_name, 
+            v.departure_point, 
+            v.destination_point, 
+            v.departure_time, 
+            v.expected_arrival_time, 
+            v.status 
+        FROM voyage_records v
+        JOIN crew_info c ON v.crew_id = c.id
+    `;
+    
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error('查询航次失败:', err);
+            return res.status(500).json({ success: false, message: '获取数据失败' });
+        }
+        res.json({ success: true, data: results });
+    });
+});
 
 });
 // 3. 启动服务器，监听 3000 端口
